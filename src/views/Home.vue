0687a487aa67c1498e5eb6f71b234d41dc56114d
Donald Gjeloshi <dongje11@htl-shkoder.com>
2020-03-22 17:59:48 +0100

    Implement Home site with two buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="home"></div>
+  <div>
+    <br />
+    <br />
+    <div class="flex-container">
+      <a href="/recorder-public/#/record">
+        <div>
+          Record
+        </div>
+      </a>
+      <a href="/recorder-public/#/upload">
+        <div>
+          Upload
+        </div>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,3 +24,25 @@ export default {
   components: {}
 };
 </script>
+<style lang="stylus" scoped>
+.flex-container
+  display: flex;
+.flex-container > a:link, a:visited
+  text-decoration none
+  color #abd1b5
+.flex-container > a:hover
+  border-radius 10px
+  background-color #094d92
+  color #95e06c
+.flex-container > a:active
+  color #094d92
+.flex-container > a > div
+  background-color: white
+  margin: 0.3rem
+  padding: 13.5rem
+  padding-bottom 19rem
+  font-size: 50px
+  border 1px solid #aaa
+  border-radius 10px
+  box-shadow 1px 1px 2px 2px #aaa
+</style>
